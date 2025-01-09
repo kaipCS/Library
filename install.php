059fcc670e1682cd6eb27404c060b61c2593f006
kaipCS <parker.ceh@oundleschool.org.uk>
2025-01-09 12:34:14 +0000

    commit

--- install.php
+++ install.php
@@ -8,7 +8,7 @@
 try {
     $conn = new PDO("mysql:host=$servername", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "CREATE DATABASE IF NOT EXISTS library;
+    $sql = "CREATE DATABASE IF NOT EXISTS library";
 
 $conn->exec($sql);
     $sql = "USE library";
