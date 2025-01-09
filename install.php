f51ac4ea4a40d84a89dd91ffe32fe2b7128771e8
kaipCS <parker.ceh@oundleschool.org.uk>
2025-01-09 12:26:32 +0000

    commit

--- install.php
+++ install.php
@@ -8,4 +8,32 @@
 try {
     $conn = new PDO("mysql:host=$servername", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "CREATE DATABASE IF NOT EXISTS Toilets";
+    $sql = "CREATE DATABASE IF NOT EXISTS library;
+
+$conn->exec($sql);
+    $sql = "USE Toilets";
+    $conn->exec($sql);
+    echo "DB created successfully";
+    $stmt1 = $conn->prepare("DROP TABLE IF EXISTS books;
+    CREATE TABLE books 
+    (isbn INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(20) NOT NULL,
+    author VARCHAR(20) NOT NULL,
+    genre VARCHAR(20) NOT NULL,
+    description VARCHAR(200) NOT NULL,
+    cover VARCHAR(20) NOT NULL,
+    onloan VARCHAR(20) NOT NULL,
+    shelf VARCHAR(20) NOT NULL,
+    fictionornot VARCHAR(20) NOT NULL)");
+    $stmt1->execute();
+    $stmt1->closeCursor(); 
+
+
+
+}
+catch(PDOException $e)
+{
+    echo $sql . "<br>" . $e->getMessage();
+}
+$conn=Null;
+?>
