a73f747bc98a9b8c8653021dbadfd129e00e8003
mazennmo <elbeltagy.m@oundleschool.org.uk>
2025-01-14 09:19:49 +0000

    addbook

---
+++ books.php
@@ -0,0 +1,10 @@
+<?php
+include_once ("connection.php");
+$stmt = $conn -> prepare("SELECT * FROM books");
+$stmt -> execute();
+while ($row = $stmt -> fetch(PDO::FETCH_ASSOC))
+{                                                     
+#print_r($row);                                                       
+echo($row["isbn"]. " ". $row["title"]." ". $row["author"]." ". $row["genre"]." ". $row["description"]." ". $row["cover"]." ". $row["onloan"]." ". $row["shelf"]." ". $row["fictionornot"]. "<br>");                                                                                                              
+}                           
+?>   
