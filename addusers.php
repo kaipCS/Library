8501e0f27005eda4b2ed651c85278ec77e042c85
kaipCS <parker.ceh@oundleschool.org.uk>
2025-01-14 09:45:38 +0000

    commit

--- addusers.php
+++ addusers.php
@@ -1,20 +1,28 @@
 <?php
+session_start();
+print_r($_SESSION);
+header('Location: usersform.php');
 print_r($_POST);
-header("Location: usersform.php");
-array_map("htmlspecialchars", $_POST);
-
-include_once("connection.php");
-$stmt = $conn->prepare("INSERT INTO users (studentnumber,surname,firstname,password,house,year ,email)
-VALUES (:studentnumber,:surname,:firstname,:password,:house,:year,:email)");
-
-$stmt->bindParam(':studentnumber', $_POST["studentnumber"]);
-$stmt->bindParam(':firstname', $_POST["firstname"]);
-$stmt->bindParam(':surname', $_POST["surname"]);
-$stmt->bindParam(':house', $_POST["house"]);
-$stmt->bindParam(':year', $_POST["year"]);
-$stmt->bindParam(':password', $_POST["password"]);
-$stmt->bindParam(':email', $_POST["email"]);
-$stmt->execute();
-$conn=null;
-
+try{
+	include_once('connection.php');
+	array_map("htmlspecialchars", $_POST);
+	
+    
+    $stmt = $conn->prepare("INSERT INTO users (studentnumber,surname,firstname,password,house,year,email)
+    VALUES (:studentnumber,:surname,:firstname,:password,:house,:year,:email)");
+    $stmt->bindParam(':studentnumber', $_POST["studentnumber"]);
+    $stmt->bindParam(':surname', $_POST["surname"]);
+    $stmt->bindParam(':firstname', $_POST["firstname"]);
+    $hashedPassword = password_hash($_POST["password"], PASSWORD_DEFAULT);
+    $stmt->bindParam(':password', $hashedPassword);
+    $stmt->bindParam(':house', $_POST["house"]);
+    $stmt->bindParam(':year', $_POST["year"]);
+    $stmt->bindParam(':email', $_POST["email"]);
+    $stmt->execute();
+}
+catch(PDOException $e)
+{
+    echo "error".$e->getMessage();
+}
+$conn=null; 
 ?>
