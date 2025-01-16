d88ef155ae80558c1f8f22c7e997f2dbc671d84d
kaipCS <parker.ceh@oundleschool.org.uk>
2025-01-16 12:15:40 +0000

    sync

--- addbook.php
+++ addbook.php
@@ -1,19 +1,19 @@
 <?php
-header('Location:booksform.php');
+#header('Location:booksform.php');
 include_once('connection.php');
 array_map("htmlspecialchars", $_POST);
 
 
-$stmt = $conn->prepare("INSERT INTO books (isbn,title,author,genre,description,cover,onloan,shelf,fictionornot )
-VALUES (null,:isbn,:title,:author,:genre,:description,:cover,:onloan,:shelf,:fictionornot)");
+$stmt = $conn->prepare("INSERT INTO books (isbn,title,author,genre,description, cover, onloan,shelf,fictionornot)
+VALUES (:isbn,:title,:author,:genre,:description, null, null, :shelf,:fictionornot)");
 $stmt->bindParam(':isbn', $_POST["isbn"]);
 $stmt->bindParam(':title', $_POST["title"]);
 $stmt->bindParam(':author', $_POST["author"]);
 $stmt->bindParam(':genre', $_POST["genre"]);
 $stmt->bindParam(':description', $_POST["description"]);
+$stmt->bindParam(':shelf', $_POST["shelf"]);
 $stmt->bindParam(':cover', $_POST["cover"]);
 $stmt->bindParam(':onloan', $_POST["onloan"]);
-$stmt->bindParam(':shelf', $_POST["shelf"]);
 $stmt->bindParam(':fictionornot', $_POST["fictionornot"]);
 
 $stmt->execute();
