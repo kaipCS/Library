840ab93f38a8da83092d88416396f038fb22c7d8
kaipCS <parker.ceh@oundleschool.org.uk>
2025-02-06 19:21:54 +0000

    returning

--- return.php
+++ return.php
@@ -1,9 +1,9 @@
 <?php
-
-header('Location:studentaccount.php');
 include_once('connection.php');
+session_start();
+$_SESSION['accountnumber'] = $_POST["accountnumber"];
+header("Location: studentaccount.php");
 array_map("htmlspecialchars", $_POST);
-
 
 $updateStmt = $conn->prepare("UPDATE books SET onloan = 0 WHERE isbn = :isbn");
 $updateStmt->bindParam(':isbn', $_POST["isbn"]);
