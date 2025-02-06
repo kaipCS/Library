e4aa14514feb4a09a408f29a449b038626996a70
kaipCS <parker.ceh@oundleschool.org.uk>
2025-02-06 12:28:09 +0000

    commit

--- addusers.php
+++ addusers.php
@@ -1,23 +1,37 @@
 <?php
 session_start();
 print_r($_SESSION);
-header('Location: usersform.php');
 print_r($_POST);
+
+header('Location: usersform.php');
+
+switch($_POST["role"]){
+    case "S":
+        $role=0;
+        break;
+    case "T":
+        $role=1;
+        break;
+}
+
+
 try{
 	include_once('connection.php');
 	array_map("htmlspecialchars", $_POST);
-	
     
-    $stmt = $conn->prepare("INSERT INTO users (accountnumber,surname,firstname,password,email)
-    VALUES (:accountnumber,:surname,:firstname,:password,:email)");
+    $stmt = $conn->prepare("INSERT INTO users (accountnumber,surname,firstname,password,email,role)
+    VALUES (:accountnumber,:surname,:firstname,:password,:email,:role)");
     $stmt->bindParam(':accountnumber', $_POST["accountnumber"]);
     $stmt->bindParam(':surname', $_POST["surname"]);
     $stmt->bindParam(':firstname', $_POST["firstname"]);
     $hashedPassword = password_hash($_POST["password"], PASSWORD_DEFAULT);
     $stmt->bindParam(':password', $hashedPassword);
     $stmt->bindParam(':email', $_POST["email"]);
+    $stmt->bindParam(':role', $role);
     $stmt->execute();
 }
+
+
 catch(PDOException $e)
 {
     echo "error".$e->getMessage();
