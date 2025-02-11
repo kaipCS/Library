1e69771c4cf62f47ed59607f37218de43bfaca7a
kaipCS <parker.ceh@oundleschool.org.uk>
2025-02-11 09:45:15 +0000

    sync

--- frontpage.php
+++ frontpage.php
@@ -1,73 +1,94 @@
 <!DOCTYPE html>
-<html lang="en">
 <head>
-<title>CSS Website Layout</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-* {
-  box-sizing: border-box;
+    <title>Front page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <style>
+        .navbar {
+            margin-bottom: 0;
+            background-color: gray;
+            border: 0;
+            font-size: 11px !important;
+            letter-spacing: 2px;
+            opacity: 1;
+            padding: 5px 10px; 
+            min-height: 40px; 
+        }
+
+        .navbar-nav > li > a,
+        .navbar .navbar-brand {
+            padding-top: 5px !important;
+            padding-bottom: 5px !important;
+            font-size: 12px;
+        }
+
+        .navbar-default .navbar-toggle {
+            border-color: transparent;
+            padding: 5px;
+        }
+
+        .navbar-form {
+            padding: 0;
+            margin: 0;
+            display: flex;
+            align-items: center;
+        }
+
+        .navbar-form input[type="text"] {
+            height: 25px;
+            font-size: 12px;
+            padding: 2px 5px;
+        }
+
+        .navbar-form button {
+            height: 25px;
+            font-size: 12px;
+            padding: 2px 8px;
+            margin-left: 5px;
+        }
+
+        .navbar li a,
+        .navbar .navbar-brand {
+            color: white !important;
+        }
+
+        .jumbotron {
+            background-color: gray;
+            color: white;
+            font-family: "Lucida Console", monospace;
+            padding: 50px 25px;
+        }
+
+        .container-fluid {
+            padding: 20px 15px;
+        }
+
+        @media screen and (max-width: 768px) {
+            .col-sm-4 {
+                text-align: center;
+                margin: 15px 0;
+            }
+        }
+        .container {
+    margin-top: 90px; 
 }
 
-body {
-  margin: 0;
-}
 
-/* Style the header */
-.header {
-  background-color: #f1f1f1;
-  padding: 20px;
-  text-align: center;
-}
+    </style>
 
-/* Style the top navigation bar */
-.topnav {
-  overflow: hidden;
-  background-color: #333;
-}
 
-/* Style the topnav links */
-.topnav a {
-  float: left;
-  display: block;
-  color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-/* Change color on hover */
-.topnav a:hover {
-  background-color: #ddd;
-  color: black;
-}
-</style>
+  <?php
+  include_once("connection.php");
+  $accountnumber = $_GET['accountnumber'];
+  $role = $_GET['role'];
+  ?>
 </head>
 <body>
 
-<div class="header">
-  <h1>Library</h1>
-</div>
 
-<div class="topnav">
-  <a href="#">Home</a>
-  <a href="#">Search</a>
-  <a href="#">Account</a>
-</div>
-
-</body>
-</html>
-    <?php
-include_once ("connection.php");
-$stmt = $conn -> prepare("SELECT * FROM books");
-$stmt -> execute();
-while ($row = $stmt -> fetch(PDO::FETCH_ASSOC))
-
-{
-    echo($row["isbn"]." ".$row["title"]." ". $row["author"]." ". $row["genre"]." ". $row["description"]." ". $row["shelf"]." ". "<br>");   
-    echo ('<img src="images/'.$row["cover"].'"><br>');
-}
-?>   
 <?php if ($role === '0') { ?>
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container-fluid">
@@ -82,7 +103,7 @@ while ($row = $stmt -> fetch(PDO::FETCH_ASSOC))
 
             <div class="collapse navbar-collapse" id="myNavbar">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="frontpage.php">Library Home</a></li>
+                    <li><a href="frontpage.php?accountnumber=<?php echo $accountnumber; ?>&role=<?php echo $role; ?>">Library home</a></li>
                     <li><a href="returnbook.php?accountnumber=<?php echo $accountnumber; ?>&role=<?php echo $role; ?>">Return</a></li>
                     <li><a href="signinform.php">Sign Out</a></li>
                 </ul>
@@ -103,6 +124,7 @@ while ($row = $stmt -> fetch(PDO::FETCH_ASSOC))
 <?php if ($role == '1') { ?>
     <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container-fluid">
+
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
                     <span class="icon-bar"></span>
@@ -114,7 +136,7 @@ while ($row = $stmt -> fetch(PDO::FETCH_ASSOC))
 
             <div class="collapse navbar-collapse" id="myNavbar">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="frontpage.php">Library Home</a></li>
+                <li><a href="frontpage.php?accountnumber=<?php echo $accountnumber; ?>&role=<?php echo $role; ?>">Library home</a></li>
                     <li><a href="returnbook.php?accountnumber=<?php echo $accountnumber; ?>&role=<?php echo $role; ?>">Return</a></li>
                     <li><a href="signinform.php">Sign Out</a></li>
                     <li><a href="booksform.php?accountnumber=<?php echo $accountnumber; ?>">Add book</a></li>
@@ -132,6 +154,8 @@ while ($row = $stmt -> fetch(PDO::FETCH_ASSOC))
     </nav>
 <?php } ?>
 
+    <div class="container">
+        <h2>Books in library </h2>
     <?php
 include_once ("connection.php");
 $stmt = $conn -> prepare("SELECT * FROM books");
@@ -140,7 +164,7 @@ while ($row = $stmt -> fetch(PDO::FETCH_ASSOC))
 
 {
     echo($row["title"]." ". $row["author"]." ". $row["genre"]." ". $row["description"]." ". $row["shelf"]." ". "<br>");   
-    echo ('<img src="images/'.$row["cover"].'"><br>');
+    echo ('<img src="images/'.$row["cover"]."ISBN: ".$row["isbn"]."<br>");
 }
 ?>   
 
