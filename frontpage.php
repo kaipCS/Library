a95fc95e1882d56255337f07e841ef16a2d9a567
mazennmo <elbeltagy.m@oundleschool.org.uk>
2025-02-11 09:27:54 +0000

    commit

--- frontpage.php
+++ frontpage.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>CSS Website Layout</title>
+<title></title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
@@ -46,7 +46,7 @@ body {
 <body>
 
 <div class="header">
-  <h1>Header</h1>
+  <h1>Library</h1>
 </div>
 
 <div class="topnav">
@@ -64,7 +64,70 @@ $stmt -> execute();
 while ($row = $stmt -> fetch(PDO::FETCH_ASSOC))
 
 {
-    echo($row["title"]." ". $row["author"]." ". $row["genre"]." ". $row["description"]." ". $row["shelf"]." ". "<br>");   
+    echo($row["isbn"]." ".$row["title"]." ". $row["author"]." ". $row["genre"]." ". $row["description"]." ". $row["shelf"]." ". "<br>");   
     echo ('<img src="images/'.$row["cover"].'"><br>');
 }
 ?>   
+<?php if ($role === '0') { ?>
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Library</a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="frontpage.php">Library Home</a></li>
+                    <li><a href="returnbook.php?accountnumber=<?php echo $accountnumber; ?>&role=<?php echo $role; ?>">Return</a></li>
+                    <li><a href="signinform.php">Sign Out</a></li>
+                </ul>
+
+                <form class="navbar-form navbar-right" action="search.php" method="get">
+                    <input type="text" name="isbnsearch" placeholder="Search by ISBN..." class="form-control">
+                    <input type="hidden" name="accountnumber" value="<?php echo $accountnumber; ?>">
+                    <input type="hidden" name="role" value="<?php echo $role; ?>">
+                    <button type="submit" class="btn btn-default">Search</button>
+                </form>
+                </form>
+            </div>
+        </div>
+    </nav>
+<?php } ?>
+
+
+<?php if ($role == '1') { ?>
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Library</a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="frontpage.php">Library Home</a></li>
+                    <li><a href="returnbook.php?accountnumber=<?php echo $accountnumber; ?>&role=<?php echo $role; ?>">Return</a></li>
+                    <li><a href="signinform.php">Sign Out</a></li>
+                    <li><a href="booksform.php?accountnumber=<?php echo $accountnumber; ?>">Add book</a></li>
+                    <li><a href="usersform.php?accountnumber=<?php echo $accountnumber; ?>">Add user</a></li>
+                </ul>
+
+                <form class="navbar-form navbar-right" action="search.php" method="get">
+                    <input type="text" name="isbnsearch" placeholder="Search by ISBN..." class="form-control">
+                    <input type="hidden" name="accountnumber" value="<?php echo $accountnumber; ?>">
+                    <input type="hidden" name="role" value="<?php echo $role; ?>">
+                    <button type="submit" class="btn btn-default">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <?php } ?>
