904dba4a6e7726c257f896be9e31eaa2d1e1cf52
kaipCS <parker.ceh@oundleschool.org.uk>
2025-02-11 09:46:34 +0000

    sync

--- frontpage.php
+++ frontpage.php
@@ -155,7 +155,7 @@
 <?php } ?>
 
     <div class="container">
-        <h2>Books in library </h2>
+        <h2>Books in library - use  </h2>
     <?php
 include_once ("connection.php");
 $stmt = $conn -> prepare("SELECT * FROM books");
@@ -163,8 +163,8 @@ $stmt -> execute();
 while ($row = $stmt -> fetch(PDO::FETCH_ASSOC))
 
 {
-    echo($row["title"]." ". $row["author"]." ". $row["genre"]." ". $row["description"]." ". $row["shelf"]." ". "<br>");   
-    echo ('<img src="images/'.$row["cover"]."ISBN: ".$row["isbn"]."<br>");
+    echo($row["title"]." ". $row["author"]." ". $row["genre"]." ". $row["description"]." ". $row["shelf"]."ISBN: ". $row["isbn"]." ". "<br>");   
+    echo ('<img src="images/'.$row["cover"].'"><br>');
 }
 ?>   
 
